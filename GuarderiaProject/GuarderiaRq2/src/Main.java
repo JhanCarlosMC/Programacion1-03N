f44578bb4ae313c7a63eaee18bde77f07e1d78d4
JhanCarlosMC <jcmartinez@uniquindio.edu.co>
2025-02-25 20:56:53 -0500

    Ejemplo de estructura de carpetas para guarderia

--- GuarderiaProject/GuarderiaRq2/src/Main.java
+++ GuarderiaProject/GuarderiaRq2/src/Main.java
@@ -2,6 +2,32 @@ import javax.swing.*;
 
 public class Main {
   public static void main(String[] args) {
+
+    pedirInformacionEstudiante();
+  }
+
+  public static void pedirInformacionEstudiante(){
+
+    Estudiante[] listEstudiante = new Estudiante[10];
+
+    for (int i = 0; i < listEstudiante.length; i++){
+      JOptionPane.showMessageDialog(null,"Ingrese la informacion del estudiante " + (i+1) + ":");
+      String nombre = JOptionPane.showInputDialog("Ingrese el nombre del estudiante: ");
+      int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad del estudiante:"));
+      String identificacion = JOptionPane.showInputDialog("Ingrese su identificacion: ");
+      String genero = JOptionPane.showInputDialog("Ingrese el genero: ");
+      String alergias = JOptionPane.showInputDialog("Ingrese las alergias: ");
+      String nombreAcudiente = JOptionPane.showInputDialog("Ingrese nombre del acudiente: ");
+      String numAcudiente = JOptionPane.showInputDialog("Ingrese numero de contacto: ");
+
+      Estudiante newEstudiante = new Estudiante(nombre,edad, identificacion,genero,alergias,nombreAcudiente,numAcudiente);
+
+      listEstudiante[i] = newEstudiante;
+    }
+
+  }
+
+  public static void ejemploArreglo(){
     int n = 4;
     int[] arregloEdades = new int[n];
 
